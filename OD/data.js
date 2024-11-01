e4a19348a89020b5bd07e63491c2f2c48d5762ab
Jeffery1470 <zyca0221@gmail.com>
2024-11-01 11:30:08 +0800

    20241101

---
+++ OD/data.js
@@ -0,0 +1,250 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "夜晚大門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7525590183638613,
+          "pitch": 0.216806278287482,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.0300842220648425,
+          "pitch": 0.1082447203013448,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3931414482238651,
+          "pitch": -0.23037887806924928,
+          "title": "門神",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 10.5pt; font-family: 新細明體, serif; letter-spacing: 0.1pt;\">湧蓮寺門外兩側各立一尊佛教護法神，左韋馱柱杵，右伽藍持槍托塔，高度近一丈，全身青銅精鑄而成，造型威嚴有力，雕塑技藝精湛，是不可多得的現代青銅塑神像作品。縱觀湧蓮寺整體建築，在現代化建築主體中，也大量保留了傳統道廟的題材來裝飾，大量採用現代式建材與裝飾手法，其中又使用不少傳統素材及匠藝，都是湧蓮寺廟宇建築中現代與傳統包容兼蓄的特色。</span><span lang=\"EN-US\"><o:p></o:p></span></p>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "夜市右側",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1404087846369118,
+          "pitch": 0.299430487359281,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.0017809482163197998,
+          "pitch": 0.08401590171055773,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1409818290078526,
+          "pitch": 0.8919647933965322,
+          "title": "路",
+          "text": "德勝路"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "右側路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.101061769321184,
+          "pitch": 0.1826429066610178,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.03918086075906402,
+          "pitch": 0.4337283914150216,
+          "title": "路",
+          "text": "德勝路和中正路交會"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "左側路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2988982698175544,
+          "pitch": 0.3849290250831725,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.0855049305492503,
+          "pitch": 0.16144574216305863,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.98025975402032,
+          "pitch": 0.4544396629555649,
+          "title": "路",
+          "text": "德勝路和成功路交會"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "左側轉角",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6229508411756548,
+          "pitch": 0.2546320623683833,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6264488693272536,
+          "pitch": 0.5075539648521179,
+          "title": "路",
+          "text": "成功路<div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
